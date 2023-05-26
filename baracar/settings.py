d5fa4ca315c31be6833e87ee5da6560d73639f6a
Shohruz <yoqutxon19832007@gmail.com>
2023-05-26 17:50:43 +0500

    third commit

--- baracar/settings.py
+++ baracar/settings.py
@@ -95,7 +95,8 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default' : dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default' : {
+        dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 }
 
 
