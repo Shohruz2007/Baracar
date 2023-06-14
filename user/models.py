8ad1a38311ee617aa20088aff70cbc6acecb7d77
Shohruz <94358926+Shohruz2007@users.noreply.github.com>
2023-06-14 18:21:39 +0500

    user '_'

--- user/models.py
+++ user/models.py
@@ -4,6 +4,8 @@ import random
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
     image = models.ImageField(upload_to='Users', null=True, blank=True)
