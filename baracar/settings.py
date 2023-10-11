08cb60e4c33388281dac23cb45634bb386373735
Shohruz <94358926+Shohruz2007@users.noreply.github.com>
2023-10-11 12:31:51 +0500

    onrender checkout

--- baracar/settings.py
+++ baracar/settings.py
@@ -102,12 +102,12 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default' :
-    #     dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default' :
+        dj_database_url.parse(env("DATABASE_URL"))
 }
 
 
